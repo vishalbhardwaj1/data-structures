d2c127b1ec187557323f781f70ac2804fbea1d6a
VB <vishalbhardwaj1@users.noreply.github.com>
2023-07-23 21:22:19 +0530

    Arrays

--- src/main/java/org/udemy/arrays/Arrays.java
+++ src/main/java/org/udemy/arrays/Arrays.java
@@ -1,2 +1,9 @@
-package org.udemy.arrays;public class Arrays {
+package org.udemy.arrays;
+public class Arrays {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
 }
