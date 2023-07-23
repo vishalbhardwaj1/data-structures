aef459df7fb6711685bf714ff64b8e1f06fc3ac3
VB <vishalbhardwaj1@users.noreply.github.com>
2023-07-23 21:45:50 +0530

    update

--- src/main/java/org/udemy/arrays/Arrays.java
+++ src/main/java/org/udemy/arrays/Arrays.java
@@ -3,7 +3,7 @@ public class Arrays {
     public static void main(String[] args) {
         int[] array = new int[100];
         for (int i = 0; i < 100; i++) {
-            System.out.println(i);
+            System.out.println("Element is " + i);
         }
     }
 }
